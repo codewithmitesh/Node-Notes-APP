13731fa2056f21789d061611f3d5277c44127b1d
Mitesh Tank <codewithmitesh@gmail.com>
2021-09-26 01:47:40 +0530

    Version 1.0

---
+++ app.js
@@ -0,0 +1,75 @@
+// We have Learned NPM installs, custom Commands Using Yargs, designing output in terminal using Chalk, JSON datafiles, Files Handeling Using 'fs' Module, and exporting functions from one file to another file, functions , Arrow functions, ES6 Short syntex helps, filter() function , find() function etc.
+
+const chalk = require("chalk");
+const yargs = require("yargs");
+const notes = require("./notes.js");
+
+// Customize yargs version
+yargs.version("1.1.0");
+
+// Create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "Note body",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler(argv) {
+    notes.addNote(argv.title, argv.body);
+  },
+});
+
+// Create remove command
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler(argv) {
+    notes.removeNote(argv.title);
+  },
+});
+
+// Create list command
+yargs.command({
+  command: "list",
+  describe: "List your notes",
+  handler() {
+    notes.listNotes();
+  },
+});
+
+// Create read command
+yargs.command({
+  command: "read",
+  describe: "Read a note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler(argv) {
+    notes.readNote(argv.title);
+  },
+});
+// console.log('hello JS Mitesh')
+yargs.parse();
+
+//to use:
+// node app.js <command> --title=" " --body=" " 
